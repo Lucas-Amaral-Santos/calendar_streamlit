1d7017c0fb0e6465b8158a6fb3689fb451832edb
lucas <amarallucas@id.uff.br>
2025-07-21 13:41:12 -0300

    Adiciona scroll

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ import pandas as pd
 import streamlit as st
 from streamlit_calendar import calendar
 from datetime import timedelta
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
 import logging
 # If you're curious of all the loggers
@@ -134,29 +140,107 @@ if uploaded:
     # --------------------------------------------------
 
 
-    professionals = sorted(df["professional"].dropna().unique())
-    selected = st.sidebar.multiselect(
-        "Filtrar por profissional", professionals, default=[]
-    )
-    filtered = df[df["professional"].isin(selected)]
+    # professionals = sorted(df["professional"].dropna().unique())
+    # selected = st.sidebar.multiselect(
+    #     "Filtrar por profissional", professionals, default=[]
+    # )
+    # filtered = df[df["professional"].isin(selected)]
 
     
-    # ---------------    # setor = sorted(df["setor"].dropna().unique())
-    # selected_setor = st.sidebar.multiselect(
+    # # ---------------    # setor = sorted(df["setor"].dropna().unique())
+    # # selected_setor = st.sidebar.multiselect(
+    # #     "Filtrar por setor", setor, default=[]
+    # # )
+    # # filtered_setor = df[df["setor"].isin(selected_setor)]
+
+    # # filtered = filtered_prof.merge(filtered_setor, how='inner')-----------------------------------
+    # # Filtro por setor
+    # # --------------------------------------------------
+    # setor = sorted(df["setor"].dropna().unique())
+    # selected = st.sidebar.multiselect(
     #     "Filtrar por setor", setor, default=[]
     # )
-    # filtered_setor = df[df["setor"].isin(selected_setor)]
+    # filtered = df[df["setor"].isin(selected)]
 
-    # filtered = filtered_prof.merge(filtered_setor, how='inner')-----------------------------------
-    # Filtro por setor
-    # --------------------------------------------------
-    setor = sorted(df["setor"].dropna().unique())
-    selected = st.sidebar.multiselect(
-        "Filtrar por setor", setor, default=[]
-    )
-    filtered = df[df["setor"].isin(selected)]
+    # filtered = filtered.merge(filtered, how='inner')
 
-    filtered = filtered_prof.merge(filtered, how='inner')
+    def filter_dataframe(df: pd.DataFrame, button_title) -> pd.DataFrame:
+        """
+        Adds a UI on top of a dataframe to let viewers filter columns
+
+        Args:
+            df (pd.DataFrame): Original dataframe
+
+        Returns:
+            pd.DataFrame: Filtered dataframe
+        """
+        modify = st.checkbox(button_title)
+
+        if not modify:
+            return df
+
+        df = df.copy()
+
+        # Try to convert datetimes into a standard format (datetime, no timezone)
+        for col in df.columns:
+            if is_object_dtype(df[col]):
+                try:
+                    df[col] = pd.to_datetime(df[col])
+                except Exception:
+                    pass
+
+            if is_datetime64_any_dtype(df[col]):
+                df[col] = df[col].dt.tz_localize(None)
+
+        modification_container = st.container()
+
+        with modification_container:
+            to_filter_columns = st.multiselect("Filtrar os atributos ", df.columns)
+            for column in to_filter_columns:
+                left, right = st.columns((1, 20))
+                # Treat columns with < 10 unique values as categorical
+                if is_categorical_dtype(df[column]) or df[column].nunique() < 100:
+                    user_cat_input = right.multiselect(
+                        f"Valores de {column}",
+                        df[column].unique(),
+                        default=list(df[column].unique()),
+                    )
+                    df = df[df[column].isin(user_cat_input)]
+                elif is_numeric_dtype(df[column]):
+                    _min = float(df[column].min())
+                    _max = float(df[column].max())
+                    step = (_max - _min) / 100
+                    user_num_input = right.slider(
+                        f"Valores de {column}",
+                        min_value=_min,
+                        max_value=_max,
+                        value=(_min, _max),
+                        step=step,
+                    )
+                    df = df[df[column].between(*user_num_input)]
+                elif is_datetime64_any_dtype(df[column]):
+                    user_date_input = right.date_input(
+                        f"Valores de {column}",
+                        value=(
+                            df[column].min(),
+                            df[column].max(),
+                        ),
+                    )
+                    if len(user_date_input) == 2:
+                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                        start_date, end_date = user_date_input
+                        df = df.loc[df[column].between(start_date, end_date)]
+                else:
+                    user_text_input = right.text_input(
+                        f"Substring ou regex em {column}",
+                    )
+                    if user_text_input:
+                        df = df[df[column].astype(str).str.contains(user_text_input)]
+
+        return df
+
+    filtered = filter_dataframe(df)
+
 
 
     # --------------------------------------------------
